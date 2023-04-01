2cfc164177c49462390d16b7f66b71aaa5867c58
Sirius Deis <falebsy@gmail.com>
2023-04-01 23:37:45 +0200

    add recognition of numbers, parentheses, signs, comma and clear while typing on bord

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ const historyEl = document.querySelector(".history"),
     inputInfoEl = document.querySelector(".input__info"),
     buttonsContainer = document.querySelector(".buttons");
 
+let data = "";
+
 buttonsContainer.addEventListener("click", (e) => {
-    const dataset = e.target.dataset;
-    const value = Object.values(dataset)[0];
-    if (!!value) return;
+    const el = e.target;
+    const dataset = Object.values(el.dataset)[0];
+    if (!dataset) return;
+    if (dataset === "number" || dataset === "parentheses" || dataset === "sign") {
+        updateData(el.textContent);
+    }
+    if (dataset === "clear") {
+        data = data.slice(0, -1);
+    }
+    if (dataset === "comma") {
+        updateData(el.textContent);
+    }
+    if (dataset === "equal") {
+        console.log("equal");
+    }
+
+    updateInputEl();
 });
 
-function updateInputEl(newData) {
-    inputPanelEl.textContent = newData;
+function updateData(value) {
+    data += value;
+}
+
+function updateInputEl() {
+    inputPanelEl.textContent = data;
 }

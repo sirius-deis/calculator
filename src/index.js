51a01ee5d9b613ae028a64beaa10127c1a7eff79
Sirius Deis <falebsy@gmail.com>
2023-04-01 23:51:34 +0200

    add recognition of root, adding it as <sup> element and clearing it

--- src/index.js
+++ src/index.js
@@ -9,14 +9,27 @@ buttonsContainer.addEventListener("click", (e) => {
     const el = e.target;
     const dataset = Object.values(el.dataset)[0];
     if (!dataset) return;
-    if (dataset === "number" || dataset === "parentheses" || dataset === "sign") {
+    if (
+        dataset === "number" ||
+        dataset === "parentheses" ||
+        dataset === "sign" ||
+        dataset === "pi" ||
+        dataset === "root"
+    ) {
         updateData(el.textContent);
     }
     if (dataset === "clear") {
-        data = data.slice(0, -1);
+        if (data.lastIndexOf("**2")) {
+            data = data.slice(0, -3);
+        } else {
+            data = data.slice(0, -1);
+        }
     }
     if (dataset === "comma") {
         updateData(el.textContent);
+    }
+    if (dataset === "**") {
+        updateData("**2");
     }
     if (dataset === "equal") {
         console.log("equal");
@@ -30,5 +43,9 @@ function updateData(value) {
 }
 
 function updateInputEl() {
-    inputPanelEl.textContent = data;
+    let dataForInsert = data;
+    if (dataForInsert.includes("**2")) {
+        dataForInsert = dataForInsert.replaceAll("**2", "<sup>2</sup>");
+    }
+    inputPanelEl.innerHTML = dataForInsert;
 }

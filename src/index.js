358fe9af6ff2ac9c42617270d2c6210b9118cda4
Sirius Deis <falebsy@gmail.com>
2023-04-02 13:49:27 +0200

    add drag movement to container

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./calculator.js";
 
+const containerEl = document.querySelector(".container");
+
 document.addEventListener("DOMContentLoaded", () => {
     const registerServiceWorker = async () => {
         if ("serviceWorker" in navigator) {
@@ -20,4 +22,27 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     registerServiceWorker();
+});
+
+let move = false;
+
+containerEl.addEventListener("mousedown", (e) => {
+    containerEl.style.cursor = "move";
+    containerEl.style.position = "absolute";
+    move = true;
+});
+containerEl.addEventListener("mousemove", (e) => {
+    if (!move) {
+        return;
+    }
+    const rect = containerEl.getBoundingClientRect();
+    let top = `${e.screenY - rect.height / 2}`;
+    let left = `${e.screenX - rect.width / 2}`;
+    containerEl.style.top = top + "px";
+    containerEl.style.left = left + "px";
+});
+containerEl.addEventListener("mouseup", (e) => {
+    containerEl.style.cursor = "auto";
+    containerEl.style.position = "relative";
+    move = false;
 });
